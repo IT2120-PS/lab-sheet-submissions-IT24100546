a953f847b8b7761224ee12b1f84b2315fa7592fa
IT24100546 <it24100546@my.sliit.lk>
2025-09-15 16:54:23 +0530

    it24100546

---
+++ Lab Sheet 06/lab6ps.R
@@ -0,0 +1,33 @@
+setwd("C:\\Users\\Tharushi\\Desktop\\pslab6")
+
+#1
+# I.  n = 50, p = 0.85
+
+##II
+pbinom(47, 50, 0.85)
+
+#2
+#I. number of calls per hour
+
+#II. Poisson Distribution -> lambda=12
+
+#III.
+dpois(15, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
